459613269d1368927e9319f00dcba7a46bee9f69
Matthew <Magg01@gmail.com>
2021-04-24 17:28:37 +0200

    updated parcelize and added dataBinding to MainActivity.kt

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,18 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         Timber.plant(Timber.DebugTree())
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/models/Shoe.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/Shoe.kt
@@ -1,7 +1,7 @@
 package com.udacity.shoestore.models
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Shoe(var name: String, var size: Double, var company: String, var description: String,
