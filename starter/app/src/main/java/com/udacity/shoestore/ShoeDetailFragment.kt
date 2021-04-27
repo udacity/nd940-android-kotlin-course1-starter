e55fa8ac8982df76beaeb7dd509fe09bbbd4758f
Matthew <Magg01@gmail.com>
2021-04-27 18:30:09 +0200

    blank shoe detail fragment added

---
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ShoeDetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ShoeDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentShoeDetailBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_shoe_detail, container, false)
+        return binding.root
+    }
+
+}
