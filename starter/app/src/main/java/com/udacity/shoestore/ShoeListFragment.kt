3c0dd302fc6a3e9c61a1a4f4a174d70acb672118
Matthew <Magg01@gmail.com>
2021-05-03 11:08:46 +0200

    shoeList added to viewModel, viewModel added to binding, first shoe in shoelist fragment gotten from shoeList

--- starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -1,16 +1,19 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
 class ShoeListFragment : Fragment() {
+
+    private lateinit var viewModel: ShoeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +24,21 @@ class ShoeListFragment : Fragment() {
         val binding: FragmentShoeListBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoe_list, container, false)
 
+
+        Log.i("ShoeListFragment", "called ViewModelProvider")
+        viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
+
+        binding.shoeViewModel = viewModel
+        binding.setLifecycleOwner(this)
+
         binding.fab.setOnClickListener(Navigation.createNavigateOnClickListener(
             R.id.action_shoeListFragment_to_shoeDetailFragment
         ))
+
         return binding.root
     }
+
+
+
 
 }
--- starter/app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
@@ -2,11 +2,20 @@ package com.udacity.shoestore
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
 
 class ShoeViewModel: ViewModel() {
 
-    init {
+    val shoeList = mutableListOf<Shoe>()
 
+    init {
+        Log.i("ShoeViewModel", "ShoeViewModel created!")
+        shoeList.add(Shoe("My first shoe", 10.0, "Nike", "Very cool", mutableListOf("")))
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("ShoeViewModel", "ShoeViewModel destroyed!")
     }
 
 }
