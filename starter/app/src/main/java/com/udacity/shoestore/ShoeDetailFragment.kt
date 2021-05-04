ab4c025e31e1e1e7f73d136645e81bbeb08e48f3
Matthew <Magg01@gmail.com>
2021-05-04 10:03:01 +0200

    cancel and save button on shoe_detail screen, cancel button navigates back

--- starter/app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
 class ShoeDetailFragment : Fragment() {
@@ -15,9 +16,15 @@ class ShoeDetailFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         val binding: FragmentShoeDetailBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoe_detail, container, false)
+
+        binding.cancelButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(
+                        R.id.action_shoeDetailFragment_to_shoeListFragment)
+        )
+
         return binding.root
     }
 
