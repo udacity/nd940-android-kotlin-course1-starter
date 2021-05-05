67dc8acbb6648f450c58073f05c8cd4ecb0bc543
Matthew <Magg01@gmail.com>
2021-05-05 10:13:07 +0200

    removed non-null assertions in shoeViewModel

--- starter/app/src/main/java/com/udacity/shoestore/models/ShoeViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/ShoeViewModel.kt
@@ -1,23 +1,21 @@
 package com.udacity.shoestore.models
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.udacity.shoestore.models.Shoe
 import timber.log.Timber
 
 class ShoeViewModel: ViewModel() {
 
-    private val _shoeList = MutableLiveData<MutableList<Shoe>>()
-    val shoeList: LiveData<MutableList<Shoe>>
+    private val mutableShoeList : MutableList<Shoe> = mutableListOf()
+    private val _shoeList = MutableLiveData<List<Shoe>>()
+    val shoeList: LiveData<List<Shoe>>
             get() = _shoeList
 
     init {
         Timber.i("ShoeViewModel created!")
-        _shoeList.value = mutableListOf()
         for(i in 1..40){
-            _shoeList.value!!.add(Shoe("My shoe $i", 10.0, "Nike", "Very cool", mutableListOf("")))
+            addShoe(Shoe("My shoe $i", 10.0, "Nike", "Very cool", mutableListOf("")))
         }
     }
 
@@ -27,7 +25,7 @@ class ShoeViewModel: ViewModel() {
     }
 
     fun addShoe(shoe : Shoe){
-        _shoeList.value!!.add(shoe)
+        mutableShoeList.add(shoe)
+        _shoeList.value = mutableShoeList
     }
-
 }
