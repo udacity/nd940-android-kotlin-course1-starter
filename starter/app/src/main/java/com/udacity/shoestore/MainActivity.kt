a43b9cadc96051585f32915fe8f1d91103014b82
Mauricio Laine <laine.mau@gmail.com>
2021-06-21 11:06:42 -0700

    Use DataBindingUtil to inflate the MainActivity layout

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,20 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
