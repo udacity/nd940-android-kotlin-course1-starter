6757ee9cc2d29e3f572ac6bdbd49c51fb5a99531
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2021-07-26 14:13:01 -0500

    Performed the initial project setup, config, and migrations. - Declared the Navigation dependencies - Added Safe Args to the project - Migrated to AndroidX - Replaced jcenter with mavenCentral - Updated Kotlin version and relevant dependencies - Migrated from Kotlin synthetics to Jetpack view binding - Switched to the kotlin-parcelize plugin - I don't like warnings. Replaced an inline calculation with a variable in a placeholder unit test (ExampleUnitTest.kt) to get rid of a warning about casting.

---
+++ app/src/test/java/com/udacity/shoestore/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.udacity.shoestore
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val four = 2 + 2
+        assertEquals(4, four)
+    }
+}
