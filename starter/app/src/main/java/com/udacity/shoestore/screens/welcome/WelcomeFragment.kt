89653e2504401fd5b7b87150498524d5ae5f1b81
Jonathan Sueck <jonathansueck@gmail.com>
2021-11-07 21:21:44 -0500

    created the welcome fragment screen that includes a welcome message, and a continue button to proceed to the instructions screen via setting a onclick listener on the continue button and using the navcontroller to go from welcome frag to the instructions fragment

--- starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.WelcomeFragmentBinding
+import com.udacity.shoestore.screens.login.LoginFragmentDirections
 
 public class WelcomeFragment : Fragment(){
     private lateinit var binding : WelcomeFragmentBinding
@@ -22,6 +24,9 @@ public class WelcomeFragment : Fragment(){
             container,
             false
         )
+        binding.btnWelcome.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment())
+        }
         return binding.root
     }
 }
