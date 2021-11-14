353729dbafccbd03e6265b0c36e3baa3a152af02
Jonathan Sueck <jonathansueck@gmail.com>
2021-11-14 13:28:44 -0500

    added in toolbar and app is running. need to adjust size of toolbar so it only takes the top portion of the screen

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,33 +2,54 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.view.Menu
+import android.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.screens.login.LoginFragmentDirections
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-        // get reference to the login views
-        val username = findViewById<EditText>(R.id.shoe_username)
-        val password = findViewById<EditText>(R.id.shoe_password)
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-        val btnRegister = findViewById<Button>(R.id.btn_register)
-//
-//        btnLogin.setOnClickListener{
-//            val username = username.text
-//            val password = password.text
-//            Toast.makeText(this@MainActivity, username, Toast.LENGTH_LONG).show()
-//        }
-//        // set onClickListener on the register button
-//        btnRegister.setOnClickListener{
-//            val username = username.text
-//            val password = password.text
-//            Toast.makeText(this@MainActivity, username, Toast.LENGTH_LONG).show()
-//        }
+
+        // Create a NavController instance
+        lateinit var navController: NavController
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // reference: https://medium.com/@ermarajhussain/how-to-work-with-navigation-controller-in-android-with-kotlin-part-2-152aa6dc3839
+        navController = Navigation.findNavController(this@MainActivity,
+            R.id.nav_host_fragment
+        )
+        appBarConfiguration = AppBarConfiguration(navController.graph,
+            drawer_layout)
+        NavigationUI.setupWithNavController(toolbar, navController,
+            appBarConfiguration)
+        NavigationUI.setupActionBarWithNavController(this, navController,
+            drawer_layout)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.activity_main_drawer, menu)
+        return true
+    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
 
 }
--- starter/app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeListingFragmentBinding
 
@@ -23,6 +24,10 @@ public class ShoeListingFragment : Fragment() {
             container,
             false
         )
-        return binding.root
+        binding.fab.setOnClickListener{
+            findNavController().navigate(ShoeListingFragmentDirections.actionShoeListingFragmentToShoeDetailFragment())
+        }
+
+            return binding.root
+        }
     }
-}
