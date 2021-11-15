010319285c2f3171800efd79bed8300d21fde8d7
Jonathan Sueck <jonathansueck@gmail.com>
2021-11-15 18:37:48 -0500

    added in a menu item for logging out, still need to add the functionality to the button

--- starter/app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/shoeListing/ShoeListingViewModel.kt
@@ -1,4 +1,11 @@
 package com.udacity.shoestore.screens.shoeListing;
 
-public class ShoeListingViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+public class ShoeListingViewModel : ViewModel() {
+    private val _currentShoes = MutableLiveData<Long>()
+    val currentShoes: LiveData<Long>
+        get() = _currentShoes
 }
