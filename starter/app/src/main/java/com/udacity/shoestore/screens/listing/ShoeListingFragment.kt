e0381cd3776a70ce23abd05ec5c11e59b6b5f2b1
Jacky <jacky.li@outlook.co.nz>
2022-10-02 06:56:24 +1300

    13. ShoeListing Fragment

--- starter/app/src/main/java/com/udacity/shoestore/screens/listing/ShoeListingFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/listing/ShoeListingFragment.kt
@@ -2,10 +2,8 @@ package com.udacity.shoestore.screens.listing
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListingBinding
@@ -23,7 +21,20 @@ class ShoeListingFragment : Fragment() {
         binding.floatingActionButton.setOnClickListener {
             onNavigateToShoeDetail()
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.listing_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+        if (item.itemId == R.id.listing_logout) {
+            findNavController().popBackStack()
+        }
     }
 
     private fun onNavigateToShoeDetail() {
