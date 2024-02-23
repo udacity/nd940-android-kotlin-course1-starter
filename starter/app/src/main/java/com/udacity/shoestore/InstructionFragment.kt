f8bacbdfbc7e6cc8f04103da256f7042205792e6
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2024-02-22 17:31:00 -0800

    Instruction screen basic layout and logic

---
+++ starter/app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
@@ -0,0 +1,23 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import timber.log.Timber
+
+class InstructionFragment  : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_instruction_destination, container, false)
+    }
+
+
+}
--- starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import timber.log.Timber
 
 class WelcomeFragment : Fragment() {
 
@@ -13,7 +16,14 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_destination, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome_destination, container, false)
+        val loginButton: Button = view.findViewById(R.id.welcome_button)
+        loginButton.setOnClickListener {
+            Timber.d("Login button clicked")
+            findNavController().navigate(R.id.action_welcome_destination_to_instructionDestination)
+        }
+
+        return view
     }
 
 
